97a91ffb8cafc7879bfa60cba33f478374bcb950
jose.monteiro <jose.monteiro@ename.pt>
2020-05-07 20:21:35 +0100

    Antti Patterns Founded

--- Tests/Tests.java
+++ Tests/Tests.java
@@ -1,10 +1,8 @@
-
 import org.junit.Test;
 
 import java.lang.reflect.Field;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 
 public class Tests {
     @Test
@@ -14,9 +12,9 @@ public class Tests {
         assertEquals("Joaquim", apresentador.getNome());
 
 
-        assertEquals("TVI", apresentador.getLocalTrabalho());
 
 
+        //Free ride Anti Pattern
     }
 
 
@@ -24,13 +22,12 @@ public class Tests {
     public void getEmail_String() throws NoSuchFieldException, IllegalAccessException{
         Apresentador apresentador= new Apresentador("Joaquim", 23, "joaquim@email.com", "TVI");
 
-        Field field = apresentador.getClass().getDeclaredField("email");
-        field.setAccessible(true);
+
 
         assertEquals("joaquim123@email.com", apresentador.getEmail());
-        assertEquals("joaquim123@email.com", field.get(apresentador));
 
 
+        //Anal probe Anti Pattern
     }
 
 
@@ -41,8 +38,9 @@ public class Tests {
         Apresentador apresentador= new Apresentador("Afonso", 32, "afonso@gmail.com", "TVI");
 
         assertEquals("Apresentador", apresentador.getLocalTrabalho());
-        assertEquals((Integer)32, apresentador.getIdade());
 
 
+        //Free ride Anti Pattern
     }
 }
+
